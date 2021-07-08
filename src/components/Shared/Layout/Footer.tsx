1b859f9e4a3324b12c7e5edd59d5ee8f56069833
Eli Pleaner <eliipleaner@gmail.com>
2021-07-08 15:03:11 +0200

    Add logo credits

--- src/components/Shared/Layout/Footer.tsx
+++ src/components/Shared/Layout/Footer.tsx
@@ -9,11 +9,22 @@ const Footer = () => {
       >
         <Facebook className="w-6 h-6 " />
       </a>
-      <div className="mb-2 prose">
-        Website by <a href="https://elipleaner.com">Eli Pleaner</a>
-      </div>
+
       <div className="prose">
         © 2021 Dharma Action Network for Climate Engagement
+      </div>
+      <div className="text-xs prose">
+        <div className="mb-2">
+          Website by <a href="https://elipleaner.com">Eli Pleaner</a>
+        </div>
+        Logo by{" "}
+        <a href="https://www.freepik.com" title="Freepik">
+          Freepik
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
       </div>
     </footer>
   );
