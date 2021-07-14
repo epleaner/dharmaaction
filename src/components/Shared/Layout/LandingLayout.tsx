7c561c1e1ceb7bb424ef1cdc7af8921d60916d40
Eli Pleaner <eliipleaner@gmail.com>
2021-07-14 20:51:30 +0200

    Too much styling

--- src/components/Shared/Layout/LandingLayout.tsx
+++ src/components/Shared/Layout/LandingLayout.tsx
@@ -3,10 +3,15 @@ import Layout from "@components/Shared/Layout";
 const index = ({ children }) => {
   return (
     <div>
-      <div className="flex flex-col justify-center h-screen p-2 font-sans text-6xl text-blue-200 uppercase text-opacity-90 dark:text-opacity-75 dark:text-blue-200 md:p-10 md:text-8xl bg-hero">
-        <p className="lg:mb-10">Dharma</p>
-        <p className="lg:mb-10">Action Network</p>
-        <p className="lg:mb-10">for Climate Engagement</p>
+      <div className="flex items-end h-screen md:p-10 bg-hero">
+        <div className="grid grid-cols-3 pb-10 pl-4 font-sans text-6xl text-blue-200 uppercase text-opacity-90 dark:text-opacity-75 dark:text-blue-200 md:text-8xl">
+          <span className="col-span-3">Dharma</span>
+          <span className="col-span-3 xl:col-span-1 xl:mr-10">Action</span>
+          <span className="col-span-3 xl:col-span-2">Network</span>
+          <span className="col-span-1 mr-10">For</span>
+          <span className="col-span-2">Climate</span>
+          <span className="col-span-3">Enagement</span>
+        </div>
       </div>
       <Layout>{children}</Layout>
     </div>
