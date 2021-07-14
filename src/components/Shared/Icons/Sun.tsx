d774d673fd3fceb9a2a3a56f06c7da75d9c71ecd
Eli Pleaner <eliipleaner@gmail.com>
2021-07-14 19:55:10 +0200

    Add night mode icons

---
+++ src/components/Shared/Icons/Sun.tsx
@@ -0,0 +1,30 @@
+function SunnyOutlineIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 512 512"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth={32}
+        d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94"
+      />
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth={32}
+        d="M336 256 A80 80 0 0 1 256 336 A80 80 0 0 1 176 256 A80 80 0 0 1 336 256 z"
+      />
+    </svg>
+  );
+}
+
+export default SunnyOutlineIcon;
--- src/components/Shared/Icons/index.tsx
+++ src/components/Shared/Icons/index.tsx
@@ -1,5 +1,7 @@
 import Facebook from "@components/Shared/Icons/Facebook";
+import Moon from "@components/Shared/Icons/Moon";
+import Sun from "@components/Shared/Icons/Sun";
 
-export { Facebook };
+export { Facebook, Sun, Moon };
 
-export default { Facebook };
+export default { Facebook, Sun, Moon };
